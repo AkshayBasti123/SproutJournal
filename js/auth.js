e4591f5b54e42a221436b03b53ffeb3d302c33fc
AkshayBasti <70245648+AkshayBasti123@users.noreply.github.com>
2025-06-30 01:22:44 -0400

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,1 +1,23 @@
+function signup() {
+  const email = document.getElementById("email").value;
+  const pass = document.getElementById("password").value;
+  const username = document.getElementById("username").value;
 
+  if (!email || !pass || !username) return alert("Please fill all fields.");
+  const userData = { email, password: pass, loginDates: [], entries: [] };
+  localStorage.setItem(`user_${username}`, JSON.stringify(userData));
+  localStorage.setItem("currentUser", username);
+  window.location.href = "dashboard.html";
+}
+
+function login() {
+  const username = document.getElementById("loginUsername").value;
+  const pass = document.getElementById("loginPassword").value;
+
+  const user = JSON.parse(localStorage.getItem(`user_${username}`));
+  if (!user || user.password !== pass) {
+    return alert("Invalid login.");
+  }
+  localStorage.setItem("currentUser", username);
+  window.location.href = "dashboard.html";
+}
