43de4d82b4f7e162b477858cb65a8833db83bb56
AkshayBasti <70245648+AkshayBasti123@users.noreply.github.com>
2025-07-01 21:32:24 -0400

    Update admin.js

--- js/admin.js
+++ js/admin.js
@@ -1,1 +1,9 @@
+import { database } from './firebase.js';
+import { ref, onValue } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
 
+const output = document.getElementById('output');
+
+onValue(ref(database, 'users'), snapshot => {
+  const data = snapshot.val();
+  output.textContent = JSON.stringify(data, null, 2);
+});
